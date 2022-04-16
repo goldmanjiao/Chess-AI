b284f7234c507fee4e56ccede98caa1594abee1f
Goldman <goldmanjiao123456@gmail.com>
2022-04-16 22:23:02 +0100

    base board

---
+++ ChessEngine.py
@@ -0,0 +1,16 @@
+class Gamestate():
+    def __init__(self):
+        #board is 8x8 2d list each element of list of 2 characters
+        self.board = [
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bp","bp","bp","bp","bp","bp","bp","bp"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["wp","wp","wp","wp","wp","wp","wp","wp"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]
+            
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
