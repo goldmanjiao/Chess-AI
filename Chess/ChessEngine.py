6af15a21afe5b5108bf4a217c68a4c20016c17c0
Goldman <goldmanjiao123456@gmail.com>
2022-04-17 17:21:20 +0100

    all possible moves for all pieces

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -141,9 +141,9 @@ class Gamestate():
     def getKingMoves(self,r,c,moves):
         kingMoves = ((-1,-1), (-1,0), (-1,1), (0,-1),(0,1),(1,-1),(1,0),(1,1))
         allyColor = "w" if self.whiteToMove else "b"
-        for m in kingMoves:
-            endRow = r + m[0]
-            endCol = c + m[1]
+        for i in range(8):
+            endRow = r + kingMoves[i][0]
+            endCol = c + kingMoves[i][1]
             if 0 <= endRow < 8 and 0 <= endCol < 8:
                 endPiece = self.board[endRow][endCol]
                 if endPiece[0] != allyColor:
